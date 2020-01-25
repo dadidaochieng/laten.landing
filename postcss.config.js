9c2ed3408ec06f37be84c3dc007000c505dec893
Odhiambo Ochieng <dadidaochieng@gmail.com>
2020-01-25 09:46:02 +0300

    Initial setup

---
+++ postcss.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  plugins: [
+    'tailwindcss',
+    process.env.NODE_ENV === 'production'
+      ? [
+          '@fullhuman/postcss-purgecss',
+          {
+            content: [
+              './src/pages/**/*.{js,jsx,ts,tsx}',
+              './src/components/**/*.{js,jsx,ts,tsx}',
+            ],
+            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+          },
+        ]
+      : undefined,
+    'autoprefixer',
+  ],
+}
